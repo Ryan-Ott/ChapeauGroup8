d0e8dbe93ea3cfa5ea5c52e342770cd25e6bf056
RyanOtt <653413@student.inholland.nl>
2020-05-10 13:48:40 +0200

    Application layers added

---
+++ ChateauROS_Group8/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public int TableID { get; set; } //from which restaurant table the order was taken
+        public int BillID { get; set; } //which bill is associated with the order
+        public int EmployeeID { get; set; } //which waiter is responsible for the order
+        public bool Completed { get; set; } //whether the order is completely finished
+        public string Comment { get; set; } //place where customer comments are stored
+    }
+}
