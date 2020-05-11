f5666553fea6d6a16d2c86dc3f356bb66b577cef
RyanOtt <653413@student.inholland.nl>
2020-05-11 23:15:10 +0200

    Test

--- ChateauROS_Group8/UserInterface/Program.cs
+++ ChateauROS_Group8/UserInterface/Program.cs
@@ -16,6 +16,7 @@ namespace UserInterface
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Order_Home());
             Application.Run(new Bill_Home());
         }
     }
