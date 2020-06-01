7bb9a225a7ae741d7edf244c2d39733006ae8937
shelbyn1122 <54841521+shelbyn1122@users.noreply.github.com>
2020-06-01 07:39:10 -0700

    update table view

--- ChateauROS_Group8/UserInterface/TableView.Designer.cs
+++ ChateauROS_Group8/UserInterface/TableView.Designer.cs
@@ -28,13 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.table1 = new System.Windows.Forms.Label();
+            this.table2 = new System.Windows.Forms.Label();
+            this.table3 = new System.Windows.Forms.Label();
+            this.table4 = new System.Windows.Forms.Label();
+            this.table5 = new System.Windows.Forms.Label();
+            this.table6 = new System.Windows.Forms.Label();
+            this.table7 = new System.Windows.Forms.Label();
+            this.table8 = new System.Windows.Forms.Label();
+            this.table9 = new System.Windows.Forms.Label();
+            this.table10 = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // table1
+            // 
+            this.table1.BackColor = System.Drawing.Color.GreenYellow;
+            this.table1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.table1.Location = new System.Drawing.Point(44, 22);
+            this.table1.Name = "table1";
+            this.table1.Size = new System.Drawing.Size(67, 64);
+            this.table1.TabIndex = 0;
+            this.table1.Text = "Table 1";
+            this.table1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.table1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // table2
+            // 
+            this.table2.BackColor = System.Drawing.Color.GreenYellow;
+            this.table2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.table2.Location = new System.Drawing.Point(161, 22);
+            this.table2.Name = "table2";
+            this.table2.Size = new System.Drawing.Size(67, 64);
+            this.table2.TabIndex = 10;
+            this.table2.Text = "Table 2";
+            this.table2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.table2.Click += new System.EventHandler(this.table2_Click);
+            // 
+            // table3
+            // 
+            this.table3.BackColor = System.Drawing.Color.GreenYellow;
+            this.table3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.table3.Location = new System.Drawing.Point(269, 22);
+            this.table3.Name = "table3";
+            this.table3.Size = new System.Drawing.Size(67, 64);
+            this.table3.TabIndex = 11;
+            this.table3.Text = "Table 3";
+            this.table3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.table3.Click += new System.EventHandler(this.table3_Click);
+            // 
+            // table4
+            // 
+            this.table4.BackColor = System.Drawing.Color.GreenYellow;
+            this.table4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.table4.Location = new System.Drawing.Point(383, 22);
+            this.table4.Name = "table4";
+            this.table4.Size = new System.Drawing.Size(67, 64);
+            this.table4.TabIndex = 12;
+            this.table4.Text = "Table 4";
+            this.table4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.table4.Click += new System.EventHandler(this.table4_Click);
+            // 
+            // table5
+            // 
+            this.table5.BackColor = System.Drawing.Color.GreenYellow;
+            this.table5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.table5.Location = new System.Drawing.Point(503, 22);
+            this.table5.Name = "table5";
+            this.table5.Size = new System.Drawing.Size(67, 64);
+            this.table5.TabIndex = 13;
+            this.table5.Text = "Table 5";
+            this.table5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.table5.Click += new System.EventHandler(this.table5_Click);
+            // 
+            // table6
+            // 
+            this.table6.BackColor = System.Drawing.Color.GreenYellow;
+            this.table6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.table6.Location = new System.Drawing.Point(44, 141);
+            this.table6.Name = "table6";
+            this.table6.Size = new System.Drawing.Size(67, 64);
+            this.table6.TabIndex = 14;
+            this.table6.Text = "Table 6";
+            this.table6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.table6.Click += new System.EventHandler(this.table6_Click);
+            // 
+            // table7
+            // 
+            this.table7.BackColor = System.Drawing.Color.GreenYellow;
+            this.table7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.table7.Location = new System.Drawing.Point(161, 141);
+            this.table7.Name = "table7";
+            this.table7.Size = new System.Drawing.Size(67, 64);
+            this.table7.TabIndex = 15;
+            this.table7.Text = "Table 7";
+            this.table7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.table7.Click += new System.EventHandler(this.table7_Click);
+            // 
+            // table8
+            // 
+            this.table8.BackColor = System.Drawing.Color.GreenYellow;
+            this.table8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.table8.Location = new System.Drawing.Point(269, 141);
+            this.table8.Name = "table8";
+            this.table8.Size = new System.Drawing.Size(67, 64);
+            this.table8.TabIndex = 16;
+            this.table8.Text = "Table 8";
+            this.table8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.table8.Click += new System.EventHandler(this.table8_Click);
+            // 
+            // table9
+            // 
+            this.table9.BackColor = System.Drawing.Color.GreenYellow;
+            this.table9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.table9.Location = new System.Drawing.Point(383, 141);
+            this.table9.Name = "table9";
+            this.table9.Size = new System.Drawing.Size(67, 64);
+            this.table9.TabIndex = 17;
+            this.table9.Text = "Table 9";
+            this.table9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.table9.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // table10
+            // 
+            this.table10.BackColor = System.Drawing.Color.GreenYellow;
+            this.table10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.table10.Location = new System.Drawing.Point(503, 141);
+            this.table10.Name = "table10";
+            this.table10.Size = new System.Drawing.Size(67, 64);
+            this.table10.TabIndex = 18;
+            this.table10.Text = "Table 10";
+            this.table10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.table10.Click += new System.EventHandler(this.table10_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(255, 218);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(100, 35);
+            this.exitButton.TabIndex = 19;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
             // 
             // TableView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(681, 383);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(633, 265);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.table10);
+            this.Controls.Add(this.table9);
+            this.Controls.Add(this.table8);
+            this.Controls.Add(this.table7);
+            this.Controls.Add(this.table6);
+            this.Controls.Add(this.table5);
+            this.Controls.Add(this.table4);
+            this.Controls.Add(this.table3);
+            this.Controls.Add(this.table2);
+            this.Controls.Add(this.table1);
             this.Name = "TableView";
             this.Text = "TableView";
             this.ResumeLayout(false);
@@ -42,5 +194,17 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Label table1;
+        private System.Windows.Forms.Label table2;
+        private System.Windows.Forms.Label table3;
+        private System.Windows.Forms.Label table4;
+        private System.Windows.Forms.Label table5;
+        private System.Windows.Forms.Label table6;
+        private System.Windows.Forms.Label table7;
+        private System.Windows.Forms.Label table8;
+        private System.Windows.Forms.Label table9;
+        private System.Windows.Forms.Label table10;
+        private System.Windows.Forms.Button exitButton;
     }
 }
--- ChateauROS_Group8/UserInterface/TableView.cs
+++ ChateauROS_Group8/UserInterface/TableView.cs
@@ -16,5 +16,60 @@ namespace UserInterface
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void table2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void table3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void table4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void table5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void table6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void table7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void table8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void table10_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
