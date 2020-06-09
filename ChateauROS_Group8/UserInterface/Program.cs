766829bedbc01b192796dedf813d2e858fb78b66
sselfie <60324267+sselfie@users.noreply.github.com>
2020-06-09 22:13:50 +0200

    Added different employees to order status and Bar, Kiitchen and waiter view.

--- ChateauROS_Group8/UserInterface/Program.cs
+++ ChateauROS_Group8/UserInterface/Program.cs
@@ -21,7 +21,7 @@ namespace UserInterface
             Application.Run(new TableView());
             Application.Run(Order_Home.GetInstance());
             Application.Run(new Bill_Home());
-            
+
         }
     }
 }
