7c259cdaf7190cc559d8cc4c32fe70be570c1e72
shelbyn1122 <54841521+shelbyn1122@users.noreply.github.com>
2020-06-12 10:00:06 -0700

    Revert "I actually don't know what I changed this time, just trying to commit changes so I can pull what I fetched"
    This reverts commit fd645350c802ee6b1c6d8044a69a04ce8678fc59.

--- ChateauROS_Group8/UserInterface/TableView.cs
+++ ChateauROS_Group8/UserInterface/TableView.cs
@@ -16,11 +16,9 @@ namespace UserInterface
     public partial class TableView : Form
     {
         Table_Service table_Service = new Table_Service();
-        OrderAndOrderItem_Service order_Service = new OrderAndOrderItem_Service();
        
         Table table;
         Employee employee;
-
         
         public TableView(Employee employee)
         {
@@ -42,7 +40,7 @@ namespace UserInterface
             List<Table> tables = table_Service.GetAllTables();
             for (int i = 0; i < tables.Count; i++)
                 switch (tables[i].TableState)
-                {
+            {
                     case TableState.available:
                         tableLabelList[i].BackColor = Color.Green;
                         break;
@@ -52,28 +50,11 @@ namespace UserInterface
                     case TableState.reserved:
                         tableLabelList[i].BackColor = Color.Yellow;
                         break;
+
+
              
                 }
-            //List<OrderItem> orderStatus = order_Service.GetOrderItems();
-            //for (int o = 0; o < orderStatus.Count; o++)
-            //    switch (orderStatus[o].OrderState)
-            //    {
-            //        case OrderState.ready:
-            //            tableLabelList[o].BackColor = Color.Red;
-            //            break;
-            //        case OrderState.preparing:
-            //            tableLabelList[o].BackColor = Color.Red;
-            //            break;
-            //        case Order.
-
-            //    }
-
-           
-        }
-
-        private void OrderStatus()
-        {
-
+            /// if tablestate.available
         }
 
         private void ChangeTableState(int tableID, Label tableLabel)
@@ -86,7 +67,6 @@ namespace UserInterface
             {
                 tableLabel.BackColor = Color.Red;
                 table.TableState = TableState.occupied;
-                
             }
             else if (dialogResult.DialogResult.Equals(DialogResult.Yes))
             {
@@ -110,11 +90,8 @@ namespace UserInterface
             }
         }
 
-        //private void AutomaticTableState(int tableID, Label 
-
         private void exitButton_Click(object sender, EventArgs e)
         {
-
             Hide();
             HomeScreen homeScreen = new HomeScreen(employee);
             homeScreen.ShowDialog();
@@ -126,7 +103,6 @@ namespace UserInterface
         private void label1_Click(object sender, EventArgs e)
         {
             ChangeTableState(1, table1);
-          
         }
 
         private void table2_Click(object sender, EventArgs e)
